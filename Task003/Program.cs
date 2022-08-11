2b0df8d7d2d96ab6c3fb1b20f925427bf8815faf
Maratius <maratius.ru@gmail.com>
2022-08-12 02:04:04 +0300

    Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+
+Console.WriteLine("Введите цифру, обозначающую день недели");
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник"); break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота"); break;
+    case 7: Console.WriteLine("Воскреснье"); break;
+    default: Console.WriteLine("Неверное число"); break;
+}
