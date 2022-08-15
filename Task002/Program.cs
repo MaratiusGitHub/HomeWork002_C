0b9d955014144c6324702ef6d46feef265b309b0
Maratius <maratius.ru@gmail.com>
2022-08-15 22:58:32 +0300

    task002 num <0

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -5,6 +5,10 @@
 
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+{
+    num = num * -1;
+}
 if (num < 100)
 {
     Console.WriteLine("Третьей цифры нет");
